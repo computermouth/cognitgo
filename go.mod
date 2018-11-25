0fdba729b65f52de3054203b52ee0cb925d0faae
computermouth <computermouth@crunchbangplusplus.org>
2018-11-24 19:36:39 -0800

    Signup works

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/computermouth/cognitgo
 
-require github.com/aws/aws-sdk-go v1.15.81
+require (
+	github.com/aws/aws-sdk-go v1.15.81
+	github.com/sethvargo/go-password v0.1.2
+	github.com/sirupsen/logrus v1.2.0
+	golang.org/x/net v0.0.0-20181114220301-adae6a3d119a // indirect
+	golang.org/x/text v0.3.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,53 +1,161 @@
 package main
 
 import (
-	"fmt"
 	"github.com/aws/aws-sdk-go/aws/session"
-	cid "github.com/aws/aws-sdk-go/service/cognitoidentity"
+	"github.com/sethvargo/go-password/password"
+	cidp "github.com/aws/aws-sdk-go/service/cognitoidentityprovider"
+	log "github.com/sirupsen/logrus"
 	"os"
+	"strings"
+	"regexp"
 )
 
-func createPool() (*cid.IdentityPool, error){
+//~ func createPool() (*cidp.IdentityPool, error){
 	
-	mySession := session.Must(session.NewSession())
-	svc := cid.New(mySession)
+	//~ var poolParams cid.CreateIdentityPoolInput
+	//~ poolParams.SetAllowUnauthenticatedIdentities(false);
 	
-	var poolParams cid.CreateIdentityPoolInput
-	poolParams.SetAllowUnauthenticatedIdentities(false);
+	//~ intClientId := os.Getenv("CLIENT")
+	//~ intProviderName := os.Getenv("PROVIDER")
+	//~ intServerSideTokenCheck := true
 	
-	intClientId := os.Getenv("CLIENT")
-	intProviderName := os.Getenv("PROVIDER")
-	intServerSideTokenCheck := true
+	//~ provider := []*cid.Provider {
+		//~ &cid.Provider {
+			//~ ClientId: &intClientId,
+			//~ ProviderName: &intProviderName,
+			//~ ServerSideTokenCheck: &intServerSideTokenCheck,
+		//~ },
+	//~ }
 	
-	provider := []*cid.Provider {
-		&cid.Provider {
-			ClientId: &intClientId,
-			ProviderName: &intProviderName,
-			ServerSideTokenCheck: &intServerSideTokenCheck,
-		},
+	//~ poolParams.SetCognitoIdentityProviders(provider)
+	//~ poolParams.SetIdentityPoolName(os.Getenv("POOL"))
+	
+	//~ pool, err := svc.CreateIdentityPool(&poolParams)
+	//~ if err != nil {
+		//~ return nil, err
+	//~ }
+	
+	//~ return pool, err
+	
+//~ }
+
+func pwMeetsCriteria(pw string) bool {
+	
+	findNum := regexp.MustCompile("[0-9]")
+	findSpecial := regexp.MustCompile("!|#|$|%|&|(|)|,|-|.|:|;|<|=|>|@|[|]|^|_|~")
+	
+	log.Debugf("pw: %s", pw)
+	
+	if pw == "" {
+		log.Debugf("pw_firstrun")
+		return false
+	} else if pw == strings.ToUpper(pw) {
+		log.Debugf("pw_noupper")
+		return false
+	} else if pw == strings.ToLower(pw) {
+		log.Debugf("pw_nolower")
+		return false
+	} else if findNum.FindAllString(pw, 1) == nil {
+		log.Debugf("pw_nonumber")
+		return false
+	} else if findSpecial.FindAllString(pw, 1) == nil {
+		log.Debugf("pw_nospecial")
+		return false
 	}
 	
-	poolParams.SetCognitoIdentityProviders(provider)
-	poolParams.SetIdentityPoolName(os.Getenv("POOL"))
+	log.Debugf("pw: %s", pw)
 	
-	pool, err := svc.CreateIdentityPool(&poolParams)
+	return true
+}
+
+func genpw() string {
+	
+	log.Debugf("genpw_start")
+	
+	pw := ""
+	var err error = nil
+	pwlen := 12
+	
+	for ! pwMeetsCriteria(pw){
+		
+		pw, err = password.Generate(pwlen, pwlen/4, pwlen/4, false, false)
+		if err != nil  {
+			panic(err)
+		}
+	
+	}
+	
+	log.Debugf("genpw_end")
+	
+	return pw
+	
+}
+
+func addUser(svc *cidp.CognitoIdentityProvider, email string, username string) (*cidp.AdminCreateUserOutput, error) {
+	
+	log.Debugf("addUser_begin")
+	
+	var acui cidp.AdminCreateUserInput
+	
+	// DesiredDeliveryMediums
+	emailDelivery := "EMAIL"
+	deliveries := []*string { &emailDelivery }
+	acui.SetDesiredDeliveryMediums(deliveries)
+	
+	// TODO: Set MessageAction to "RESEND" for
+	// already existing users
+	
+	// TemporaryPassword
+	acui.SetTemporaryPassword(genpw())
+	
+	// UserAttributes
+	uaName := "email"
+	uaValue := email
+	ua := cidp.AttributeType {
+		Name: &uaName,
+		Value: &uaValue,
+	}
+	acui.SetUserAttributes( []*cidp.AttributeType{ &ua } )
+	
+	// UserPoolId
+	acui.SetUserPoolId( os.Getenv("POOL") )
+	
+	// Username
+	acui.SetUsername(username)
+	
+	out, err := svc.AdminCreateUser(&acui)
 	if err != nil {
-		return nil, err
+		return out, err
 	}
 	
-	return pool, err
+	log.Debugf("addUser_end")
 	
+	return out, err
 }
 
 func main(){
 	
-	fmt.Println("start")
-	
-	pool, err := createPool()
-	if err != nil {
-		panic(err)
+	if os.Getenv("DEBUG") == "1" {
+		log.SetLevel(log.DebugLevel)
+	} else {
+		log.SetLevel(log.WarnLevel)
 	}
 	
-	fmt.Printf("%+v\n", pool)
+	log.Debugf("main_start")
+	
+	if len(os.Args) < 3 || len(os.Args) > 3 {
+		log.Errorf("%s [email] [username]", os.Args[0])
+		os.Exit(1)
+	}
+	
+	mySession := session.Must(session.NewSession())
+	svc := cidp.New(mySession)
+	
+	out, err := addUser(svc, os.Args[1], os.Args[2])
+	if err != nil {
+		log.Errorf("%+v", err)
+	}
+	
+	log.Debugf("addUser out:\n%+v", out)
 	
 }
